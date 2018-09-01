eb75b16ae9458eb9ec432dc6c092e286147eab92
shis63032 <shis63032@his-student.org>
2018-09-01 17:20:14 +0800

    Changed to "Press enter to continue."

--- CC Initiates Ready.c
+++ CC Initiates Ready.c
@@ -10,7 +10,7 @@ int Program_Check();
 //소개 함수 - 기본 시작 함수
 int main() {
   int selection;
-
+  system("clear");
   printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
          "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
   printf("Greetings, Users. I'm shis63032. One of the elders in 2018-2019 "
@@ -29,7 +29,8 @@ int main() {
 
   switch (selection) {
   case 1:
-    printf("\n\nGood. Now, we will proceed to the next step.\n");
+    printf("\n\nGood. Now, we will proceed to the next step.\n\n");
+
     system("read -p 'Press enter to continue'");
     selection = CLT_Installation();
     break;
@@ -42,8 +43,12 @@ int main() {
            "20170529233353_yecyhdcp.jpg");
     break;
 
+  case 0:
+    selection = Program_Check();
+    break;
+
   default:
-    selection = main();
+    system("read -p 'Press enter to continue.'");
     break;
   }
 
@@ -53,13 +58,16 @@ int main() {
 // CLT 설치 함수
 int CLT_Installation() {
   int selection;
-  printf(
-      "\nNow, we are going to install the infamous XCODE Command Line Tool.\n");
-  printf("This tool will let you to approach to using programming language in "
-         "your terminal.\n");
+  system("clear");
+  printf("Now, we are going to install the infamous XCODE Command Line Tool.\n");
+  printf("This tool will let you to approach various background set-ups that requires the programming languages to work in "
+   "your terminal.\n");
   printf("Detailed information will not be given right now due to time "
-         "constrain and the level of difficulty. But you need it.\n\n\n");
+         "constrain and due to the high level of difficulty. But you need it.\n\n");
+
   printf("The download can be quite long, so go have some tea...\n\n\n");
+
+
   system("xcode-select --install");
 
   system("read -p 'Press enter to continue.'");
@@ -71,28 +79,19 @@ int CLT_Installation() {
 // Homebrew 다운 함수 - 제 2 차 다운 함수.
 int Homebrew_Installation() {
   int selection;
-  printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
-         "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
-
-  printf("Great. Now lets install the Homebrew!\n");
-  printf("Homebrew will allow you to download useful things without "
-         "downloading by your web browser.\n");
-  printf("From now on, you can download necessary things in your "
-         "\"Terminal\".\n\n");
-    system(
-        "/usr/bin/ruby -e \"$(curl -fsSL "
-        "https://raw.githubusercontent.com/Homebrew/install/master/install)\"");
-
-
   system("clear");
   printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
          "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
 
-  printf("Great. Now lets install the Homebrew!\n");
-  printf("Homebrew will allow you to download useful things without "
-         "downloading by your web browser.\n");
+  printf("Great. Now lets install Homebrew!\n");
+  printf("Homebrew will allow you to download useful things in Mac OS without "
+         "downloading by your web browser or App Store.\n");
   printf("From now on, you can download necessary things in your "
          "\"Terminal\".\n\n");
+
+    system(
+        "/usr/bin/ruby -e \"$(curl -fsSL "
+        "https://raw.githubusercontent.com/Homebrew/install/master/install)\"");
 
   system("read -p 'Press enter to continue.'");
   selection = Atom_Installation();
@@ -102,41 +101,27 @@ int Homebrew_Installation() {
 
 int Atom_Installation(int x) {
   int selection;
+  system("clear");
   printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
          "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
 
-  printf("Great work! Now lets install the power of Atom!\n");
+  printf("Great work! Now, we have to face the power of Atom!\n");
   printf("Atom is a good program that can let you program in an environment "
          "that you can set by yourself.\n");
-  printf("You always can customize your workspace - atom - to your favor.\n");
-  printf("In atom, you can install the tools and themes made by others, or you "
-         "can even create your own\n");
+  printf("You always can customize your workspace - Atom - to your favor without limit.\n");
+  printf("In Atom, you can install the tools and themes made by others, or you "
+         "can even create your own tools, application, or theme.\n");
   printf("(I personally do not recommend, unless you have a solid skill for "
          "Web Designing Languages and some sweet senses.)\n\n");
-
-  printf("Anyway, after the atom is installed, I recommend you to go in and "
-         "look around the commands.\n");
 
   system("brew cask install atom");
-  system("clear");
 
-  printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
-         "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
+  printf("\n\nAnyway, when Atom is installed, I recommend you to go in and "
+         "look around the commands.\n\n");
 
-  printf("Great work! Now lets install the power of Atom!\n");
-  printf("Atom is a good program that can let you program in an environment "
-         "that you can set by yourself.\n");
-  printf("You always can customize your workspace - atom - to your favor.\n");
-  printf("In atom, you can install the tools and themes made by others, or you "
-         "can even create your own\n");
-  printf("(I personally do not recommend, unless you have a solid skill for "
-         "Web Designing Languages and some sweet senses.)\n\n");
-
-  printf("Anyway, after the atom is installed, I recommend you to go in and "
-         "look around the commands.\n");
-  // system("atom");
 
   system("read -p 'Press enter to continue.'");
+
   selection = AtomPackage_Installation();
 
   return 0;
@@ -144,17 +129,19 @@ int Atom_Installation(int x) {
 
 int AtomPackage_Installation(int x) {
   int selection;
+  system("clear");
   printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
          "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
 
   printf("We are almost done. Have you looked around inside the Atom?\n");
   printf("Well... If there is 'only' atom, it will be just a boring program "
-         "that can do nothing.\n");
+         "that can do nothing at all.\n");
   printf("So, I will install a small terminal and some complier utilities to "
-         "your atom.\n");
+         "your atom as a package.\n");
   printf("The terminal that I will install will have same function as this "
-         "terminal right now.\n");
-  printf("But isn't just easy to let terminal pop up inside the atom?\n");
+         "terminal that you are using it right now.\n");
+  printf("While you program by using atom, most likly you will find bit annoyed that the terminal and the atom is splited.\n");
+  printf("\"Isn't it easy to just let terminal pop up inside Atom?\" You may say,\n");
   printf("So someone actually made a package for that.\n\n");
 
   system("apm install platformio-ide-terminal");
@@ -167,9 +154,8 @@ int AtomPackage_Installation(int x) {
   system("apm install script");
 
   printf("\n\nAlthough, 'script' package requires some other programs to build "
-         "your codes depending on the language you use.\n(And there are a lot "
-         "of different languages.)\n");
-  printf("Now, lets check if there are necessary programs in your computer.\n");
+         "your codes depending on the language you use.\nEach conditions and requirements are user's work that need to be installed before the actual usage.\nYou have to know that; the programmers arn't kind to help you to build the whole program just like the 'Installation Package' like this one.\nYOU ARE THE ONE THAT SHOULD ACTUALLY FIGURE OUT THE SYSTEM AND BUILD THE ENVIRONMENT!\n\n");
+  printf("Now, lets check if there are necessary programs in your computer.\n\n");
 
   system("read -p 'Press enter to continue.'");
   selection = Program_Check();
@@ -179,7 +165,7 @@ int AtomPackage_Installation(int x) {
 
 int Program_Check() {
   int selection;
-
+  system("clear");
   printf("Copyright (c) 2018 Copyright HIS Computer Club All Rights "
          "Reserved.\nHIS Computer Club Package Checking Program.\n\n\n");
 
@@ -190,6 +176,6 @@ int Program_Check() {
   system("atom -v");
   printf("\nCheck completed! You are good to go!\n\n");
   system("read -p 'Press enter to end this session.'");
-  
+
   return 0;
 }
